08a3393a14295f6076dd85b2bfabc0b0da722104
sathishp4223 <sathishp4223@gmail.com>
2026-04-26 14:20:02 +0530

    vendor file test

--- labs/htdocs/index.php
+++ labs/htdocs/index.php
@@ -125,7 +125,7 @@ ob_start();
                     <span
                         id="multi-link"
                         style="color: Orange; cursor: pointer;">
-                        SELFMADE NINJA ACADEMY test over
+                        SELFMADE NINJA ACADEMY again over
                     </span>
                 </p>
                 <div class="hero-button-group">
