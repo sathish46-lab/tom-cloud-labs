34d9c1b824796b7fb6b0965454e62ec1626d1bdc
sathishp4223 <sathishp4223@gmail.com>
2026-06-17 20:48:58 +0530

    update README.md

--- labs/htdocs/debug_quiz.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'src/load.php';
-$db = DatabaseConnection::getDefaultDatabase();
-$quiz = $db->quizzes->findOne();
-echo json_encode($quiz, JSON_PRETTY_PRINT);
--- labs/htdocs/test_subs.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once 'src/load.php';
-$subs = \TomLabs\Labs\Quiz::getSubtopicsForCategory('c-programming');
-echo "Found " . count($subs) . " subtopics for c-programming.\n";
-foreach ($subs as $s) {
-    echo "- " . $s['title'] . " (" . $s['id'] . ")\n";
-}
