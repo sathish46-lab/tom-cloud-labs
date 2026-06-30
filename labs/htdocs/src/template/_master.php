c23d4d12c51c9661c423fc2c49c3b4cd73215f55
sathishp4223 <sathishp4223@gmail.com>
2026-06-30 15:55:28 +0530

    Microsoft Clarity update

--- labs/htdocs/src/template/_master.php
+++ labs/htdocs/src/template/_master.php
@@ -22,7 +22,7 @@ define('PAGE_START_TIME', microtime(true));
                 c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                 t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                 y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "n7q1nqtm06");
+            })(window, document, "clarity", "script", "xf2vp1zlg2");
         }, 2000); 
     });
     </script>
